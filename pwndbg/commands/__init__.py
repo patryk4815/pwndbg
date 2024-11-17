58d7062d42ac30818a74a3b2a72d64fb33a92133
patryk4815 <patryk.sondej@gmail.com>
2024-11-17 07:41:36 +0100

    commands/plist.py: Move into agnostic

--- pwndbg/commands/__init__.py
+++ pwndbg/commands/__init__.py
@@ -709,7 +709,6 @@ def load_commands() -> None:
         import pwndbg.commands.onegadget
         import pwndbg.commands.pcplist
         import pwndbg.commands.peda
-        import pwndbg.commands.plist
         import pwndbg.commands.reload
         import pwndbg.commands.rop
         import pwndbg.commands.ropper
@@ -756,6 +755,7 @@ def load_commands() -> None:
     import pwndbg.commands.plist
     import pwndbg.commands.probeleak
     import pwndbg.commands.procinfo
+    import pwndbg.commands.plist
     import pwndbg.commands.radare2
     import pwndbg.commands.retaddr
     import pwndbg.commands.rizin
