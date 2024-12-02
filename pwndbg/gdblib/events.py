dbbe5a6967ba426089f6e71ebae2e27654df3fbf
patryk4815 <patryk.sondej@gmail.com>
2024-12-02 03:26:14 +0100

    Test workaround xd

--- pwndbg/gdblib/events.py
+++ pwndbg/gdblib/events.py
@@ -280,6 +280,7 @@ def invoke_event(event: Any, *args: Any, **kwargs: Any) -> None:
             print('IS_SAFE', str(event), str(args), _is_safe_event_thread())
             # raise RuntimeError('PWNDBG nie wspiera "commands" jest to bug w gdb')
             print('TREAHD?', threading.main_thread(), lock.locked())
+            print('TREAHD?', threading.main_thread(), lock.locked(), threading.current_thread())
 
         detect_another_thread_issue = True
         lock.acquire()
