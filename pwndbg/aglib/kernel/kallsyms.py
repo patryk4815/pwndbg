d69e6fd692c13f26420587f4035cce2fcfde49bf
patryk4815 <patryk.sondej@gmail.com>
2024-12-17 02:48:14 +0100

    port kernel xd

--- pwndbg/aglib/kernel/kallsyms.py
+++ pwndbg/aglib/kernel/kallsyms.py
@@ -45,6 +45,8 @@ class Kallsyms:
         self.kbase = pwndbg.aglib.kernel.kbase()
 
         mapping = pwndbg.aglib.kernel.get_first_kernel_ro()
+        assert mapping is not None, "kernel memory mappings are missing"
+
         self.r_base = mapping.vaddr
         self.kernel_ro_mem = pwndbg.aglib.memory.read(mapping.vaddr, mapping.memsz)
 
