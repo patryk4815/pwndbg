0d1aa2343da1f5311683f22971370f6e994b67ed
patryk4815 <patryk.sondej@gmail.com>
2024-12-17 02:03:00 +0100

    fix typos

--- pwndbg/dbg/lldb/__init__.py
+++ pwndbg/dbg/lldb/__init__.py
@@ -950,7 +950,10 @@ class LLDBProcess(pwndbg.dbg_mod.Process):
                 pass
 
         # Second, try to do a binary search for the limit of the range.
-        def test(s):
+        def test(s: int):
+            if s == 0:
+                return False
+
             b = self.process.ReadMemory(address, s, e)
             return b is not None
 
